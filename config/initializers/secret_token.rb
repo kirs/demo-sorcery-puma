d3f225a21f1c8226d719c25b80aa1b1d2abaedb8
Kir Shatrov <shatrov@me.com>
2013-11-04 00:50:59 +0400

    Demo sorcery app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SorceryDemo::Application.config.secret_key_base = '0ded4b18652c8f8fec5ca26c95b1e851bf8601852d27383233bdb8dff55f1e2a761422aeca64e2051fb1b801c5f2f11dbffd305d353e7ac331eecada49b0e461'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SorceryDemo::Application.config.session_store :cookie_store, key: '_sorcery-demo_session'
